a1e2d898926a5e26f38711ab28d94ac47b477b1f
Praneeth <praneethcc@gmail.com>
2020-11-10 11:59:29 +0530

    add project

---
+++ Vehical_ParkingManagementSystem/app/src/main/java/com/example/praneeth/vehical_parkingmanagementsystem/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.praneeth.vehical_parkingmanagementsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
